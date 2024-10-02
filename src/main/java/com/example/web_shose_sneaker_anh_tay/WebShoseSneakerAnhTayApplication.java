280848c18209397fd27f81d21f13db0dedcad522
namtp04 <namtran14022004@gmail.com>
2024-10-02 09:46:17 +0700

    ok

---
+++ src/main/java/com/example/web_shose_sneaker_anh_tay/WebShoseSneakerAnhTayApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_shose_sneaker_anh_tay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShoseSneakerAnhTayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShoseSneakerAnhTayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_shose_sneaker_anh_tay/WebShoseSneakerAnhTayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_shose_sneaker_anh_tay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShoseSneakerAnhTayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
